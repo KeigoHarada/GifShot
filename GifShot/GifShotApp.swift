c0614f137791687207c8d5786e0a5f1e1a1f9b96
KeigoHarada <hkddna5@gmail.com>
2025-09-22 23:09:18 +0900

    Initial Commit

---
+++ GifShot/GifShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GifShotApp.swift
+//  GifShot
+//
+//  Created by 原田啓吾 on 2025/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GifShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
